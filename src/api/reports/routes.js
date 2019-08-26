025e16f7e5e2b073ca655f121fbdde276a03e21f
Rodrigo Wippel <rodrigow1992@gmail.com>
2019-08-25 21:32:09 -0300

    report route abastecimentos

--- src/api/reports/routes.js
+++ src/api/reports/routes.js
@@ -4,6 +4,6 @@ const ReportsController = require('./controller')
 
 const router = express.Router()
 
-router.get('/supplies', ReportsController.getAllSupplies)
+router.get('/abastecimentos', ReportsController.getAllSupplies)
 
 module.exports = router
