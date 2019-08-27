9f700529c42003422cf23c7f604aff09498ae2d9
Rodrigo Wippel <rodrigow1992@gmail.com>
2019-08-27 16:56:20 -0300

    timeZone

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -7,9 +7,13 @@ const formatHour = date => {
 const formatDate = (date, daysToAdd) => {
   const dateFormat = 'DD/MM/YYYY'
   if (daysToAdd) {
-    return format(addDays(date, daysToAdd), dateFormat)
+    return format(addDays(date, daysToAdd), dateFormat, {
+      timeZone: 'America/Sao_Paulo'
+    })
   }
-  return format(date, dateFormat)
+  return format(date, dateFormat, {
+    timeZone: 'America/Sao_Paulo'
+  })
 }
 
 module.exports = {
