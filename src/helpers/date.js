fcd8f6fa1b7278e37520adc343ef9591e24d055b
Rodrigo Wippel <rodrigow1992@gmail.com>
2019-08-27 17:05:21 -0300

    remove timezone config

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -1,21 +1,15 @@
 const { format, addDays } = require('date-fns')
 
 const formatHour = date => {
-  return format(date, 'HH:mm', {
-    timeZone: 'America/Sao_Paulo'
-  })
+  return format(date, 'HH:mm')
 }
 
 const formatDate = (date, daysToAdd) => {
   const dateFormat = 'DD/MM/YYYY'
   if (daysToAdd) {
-    return format(addDays(date, daysToAdd), dateFormat, {
-      timeZone: 'America/Sao_Paulo'
-    })
+    return format(addDays(date, daysToAdd), dateFormat)
   }
-  return format(date, dateFormat, {
-    timeZone: 'America/Sao_Paulo'
-  })
+  return format(date, dateFormat)
 }
 
 module.exports = {
