674062ee02fb41ea51ccfc7874db2ab7e5a98656
Rodrigo Wippel <rodrigow1992@gmail.com>
2019-08-31 12:07:37 -0300

    crud configurations

--- src/api/configurations/controller.js
+++ src/api/configurations/controller.js
@@ -1,4 +1,16 @@
-const { Configuration } = require('../models')
+const { Configuration, Company, GasStation } = require('../models')
+
+const normalizeResponse = (configuration) => ({
+  id: configuration.id,
+  combustivel: configuration.combustivel,
+  valorVenda: configuration.valorVenda,
+  taxaGasola: configuration.taxaGasola,
+  prazoPagamentoGasola: configuration.prazoPagamentoGasola,
+  prazoPagamentoCliente: configuration.prazoPagamentoCliente,
+  desconto: configuration.desconto,
+  empresa: configuration.company.nome,
+  posto: configuration.gasStation.nome
+})
 
 module.exports = {
   create: async (req, res) => {
@@ -25,6 +37,60 @@ module.exports = {
     })
 
     res.send(configuration)
+  },
+
+  update: async (req, res) => {
+    const {
+      combustivel,
+      valorVenda,
+      taxaGasola,
+      prazoPagamentoGasola,
+      prazoPagamentoCliente,
+      desconto,
+      idPosto,
+      idEmpresa
+    } = req.body
+    const { configurationId } = req.params
+
+    const configuration = await Configuration.findOne({
+      include: [Company, GasStation],
+      where: { id: configurationId }
+    })
+
+    const configurationUpdated = await configuration.update({
+      combustivel,
+      valorVenda,
+      taxaGasola,
+      prazoPagamentoCliente,
+      prazoPagamentoGasola,
+      desconto,
+      gasStationId: idPosto,
+      companyId: idEmpresa
+    })
+
+    res.send(normalizeResponse(configurationUpdated))
+  },
+
+  delete: async (req, res) => {
+    const { configurationId } = req.params
+
+    await Configuration.destroy({
+      where: { id: configurationId }
+    })
+
+    res.send('Removido com sucesso.')
+  },
+
+  getAll: async (_, res) => {
+    const configurations = await Configuration.findAll({
+      include: [Company, GasStation]
+    })
+
+    const normalizedResponse = configurations.map(configuration => (
+      normalizeResponse(configuration)
+    ))
+
+    res.send(normalizedResponse)
   },
 
   getConfiguration: async ({ fuelType, companyId, gasStationId }) => {
--- src/api/configurations/routes.js
+++ src/api/configurations/routes.js
@@ -4,6 +4,9 @@ const ConfigurationController = require('./controller')
 
 const router = express.Router()
 
+router.get('/', ConfigurationController.getAll)
 router.post('/', ConfigurationController.create)
+router.put('/:configurationId', ConfigurationController.update)
+router.delete('/:configurationId', ConfigurationController.delete)
 
 module.exports = router
